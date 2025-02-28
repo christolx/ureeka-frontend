647b879ffe312cb3da71859bff13904ebd439ec8
christolx <81254243+christolx@users.noreply.github.com>
2025-02-28 18:58:39 +0700

    Added ShadCN

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import './App.css';
+import {Button} from "@/components/ui/button.tsx";
 
 function App() {
     const [count, setCount] = useState(0);
@@ -7,14 +8,9 @@ function App() {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen">
             <h1 className="text-3xl font-bold mb-4">Food Website</h1>
-            <div className="p-4 border rounded shadow-md">
-                <button
-                    className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-                    onClick={() => setCount((count) => count + 1)}
-                >
-                    Clicked me {count} times.
-                </button>
-            </div>
+            <Button onClick={()=>setCount(dynamicNumber=>dynamicNumber+1)}>
+                Clicked me {count} times.
+            </Button>
         </div>
     );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import * as path from "node:path";
 
 // https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [
-      tailwindcss(),
-      react()
-  ],
+    plugins: [react(), tailwindcss()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
 })
