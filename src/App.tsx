92ab6716dfe55be2eaa09b96e59a4871c82602c7
Winona <winona.p.pangestu@gmail.com>
2025-03-02 16:55:59 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
-import { useState } from 'react';
 import './App.css';
-import {Button} from "@/components/ui/button.tsx";
+import Navbar from './components/Navbar';
+// import { useState } from 'react';
+// import {Button} from "@/components/ui/button.tsx";
 
-function App() {
-    const [count, setCount] = useState(0);
+const App = () => {
+    // const [count, setCount] = useState(0);
 
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen">
-            <h1 className="text-3xl font-bold mb-4">Food Website</h1>
-            <Button onClick={()=>setCount(dynamicNumber=>dynamicNumber+1)}>
-                Clicked me {count} times.
-            </Button>
-        </div>
+        // <div className="flex flex-col items-center justify-center min-h-screen">
+        //     <h1 className="text-3xl font-bold mb-4">Food Website</h1>
+        //     <Button onClick={()=>setCount(dynamicNumber=>dynamicNumber+1)}>
+        //         Clicked me {count} times.
+        //     </Button>
+        // </div>
+        <main className='relative min-h-screen w-screen overflow-x-hidden'>
+            <Navbar />
+        </main>
     );
 }
 
