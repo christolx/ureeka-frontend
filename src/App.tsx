64844090ea0851c7403651bb57d07c158bd5fb65
Winona <winona.p.pangestu@gmail.com>
2025-03-08 22:11:00 +0700

    Added Router for page directories (esp. LoginPage.tsx)

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,46 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Donations from "./components/Donations";
 import Articles from "./components/Articles";
 import Footer from "./components/Footer";
 import About from "./components/About";
+import LoginPage from "./pages/LoginPage"; // Import your LoginPage component
 
 const App = () => {
     return (
-        <main className="relative min-h-screen w-screen overflow-x-hidden bg-[#FFFFFF] pt-16">
-            <Navbar />
+        <Router>
+            <main className="relative min-h-screen w-screen overflow-x-hidden bg-[#FFFFFF] pt-16">
+                <Navbar />
 
-            <section id="home">
-                <Hero />
-            </section>
+                <Routes>
+                    <Route path="/" element={
+                        <>
+                            <section id="home">
+                                <Hero />
+                            </section>
 
-            <section id="about">
-                <About />
-            </section>
+                            <section id="about">
+                                <About />
+                            </section>
 
-            <section id="donations">
-                <Donations />
-            </section>
-            <section id="articles">
-                <Articles />
-            </section>
-            <Footer />
-        </main>
+                            <section id="donations">
+                                <Donations />
+                            </section>
+
+                            <section id="articles">
+                                <Articles />
+                            </section>
+
+                            <Footer />
+                        </>
+                    } />
+
+                    <Route path="/login" element={<LoginPage />} />
+                </Routes>
+            </main>
+        </Router>
     );
 };
 
