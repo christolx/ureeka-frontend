400484c9dc84b9cf194e034a284feb60b864a4e8
melissalau17 <melissalau17300@gmail.com>
2025-03-15 22:04:26 +0700

    edited footer layout for mobile

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -38,40 +38,37 @@ interface NavigationItem {
   const Footer: React.FC = () => {
     return (
       <footer className="bg-[#6E826D] text-white text-center py-6 mt-16">
-        <div className="max-w-5xl mx-auto">
+        <div className="max-w-5xl mx-auto px-6">
           <div className="w-full py-10 lg:py-20 text-background">
             <div className="container mx-auto">
-              <div className="grid lg:grid-cols-2 gap-10 items-center">
-                <div className="flex gap-8 flex-col items-start">
-                  <div className="flex gap-2 flex-col">
-                    <h2 className="text-3xl md:text-5xl tracking-tighter max-w-xl font-regular text-left">
+              <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-10 items-center">
+                <div className="flex gap-8 flex-col sm:items-center lg:items-start">
+                  <div className="flex gap-2 flex-col text-center lg:text-left">
+                    <h2 className="text-3xl md:text-5xl tracking-tighter max-w-xl font-regular">
                       <b>FoodFund</b>
                     </h2>
-                    <p className="text-lg max-w-lg leading-relaxed tracking-tight text-background/75 text-left">
+                    <p className="text-lg max-w-lg leading-relaxed tracking-tight text-background/75">
                       Feeding <span className="font-medium"><i>HOPE</i></span>, one meal at a time.
                     </p>
                   </div>
-                  <div className="flex gap-20 flex-row">
-                    <div className="flex flex-col text-sm max-w-lg leading-relaxed tracking-tight text-background/75 text-left">
+                  <div className="flex flex-row sm:flex-row justify-center lg:justify-start gap-10 sm:gap-5">
+                    <div className="flex flex-col text-sm leading-relaxed tracking-tight text-background/75">
                       <p>Jl H Saidi 5/8</p>
                       <p>Sumatera Utara</p>
                       <p>Medan 20112</p>
                     </div>
-                    <div className="flex flex-col text-sm max-w-lg leading-relaxed tracking-tight text-background/75 text-left">
+                    <div className="flex flex-col text-sm leading-relaxed tracking-tight text-background/75">
                       <a href="/terms">Terms of service</a>
                       <a href="/privacy">Privacy Policy</a>
                     </div>
                   </div>
                 </div>
-                <div className="grid lg:grid-cols-3 gap-10 items-start">
+                <div className="grid grid-cols-3 gap-10 items-start text-left">
                   {navigationItems.map((item) => (
-                    <div
-                      key={item.title}
-                      className="flex text-base gap-1 flex-col items-start"
-                    >
+                    <div key={item.title} className="flex text-base gap-1 flex-col items-start">
                       <div className="flex flex-col gap-2">
                         {item.href ? (
-                          <a href={item.href} className="flex justify-between items-center text-xl">
+                          <a href={item.href} className="text-xl">
                             {item.title}
                           </a>
                         ) : (
@@ -79,11 +76,7 @@ interface NavigationItem {
                         )}
                         {item.items &&
                           item.items.map((subItem) => (
-                            <a
-                              key={subItem.title}
-                              href={subItem.href}
-                              className="flex justify-between items-center text-background/75"
-                            >
+                            <a key={subItem.title} href={subItem.href} className="text-background/75">
                               {subItem.title}
                             </a>
                           ))}
@@ -100,4 +93,3 @@ interface NavigationItem {
   };
   
   export default Footer;
-  
